9ee2c0094afd6c8ec0f9044552a829c1335edfcb
bulatgabdullin <112638878+byjiat@users.noreply.github.com>
2023-06-29 12:52:17 +0500

    Add permissions in users app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,3 +125,16 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
