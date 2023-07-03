c6f312fa45f76c417decd23ed069376fdafbdb20
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-07-03 17:16:21 +0500

    Add models shopingcart, favorites

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -39,6 +39,14 @@ class Ingredient(models.Model):
         return f'{self.name[:TEXT_LENGTH]}'
 
 
+class RecipeManager(models.Manager):
+    def get_recipes(self):
+        return self.all().select_related('author')
+
+    def get_recipes_count(self):
+        return self.all().select_related('author').count()
+
+
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
@@ -47,7 +55,8 @@ class Recipe(models.Model):
     )
     ingredients = models.ManyToManyField(
         Ingredient,
-        through='RecipeIngredient'
+        through='RecipeIngredient',
+        related_name='%(app_label)s_%(class)s',
     )
     tags = models.ForeignKey(
         Tag,
@@ -65,6 +74,8 @@ class Recipe(models.Model):
         help_text=_('Cooking time in minutes'),
     )
 
+    objects = RecipeManager()
+
     class Meta:
         verbose_name = _('Recipe')
         verbose_name_plural = _('Recipes')
@@ -77,8 +88,16 @@ class Recipe(models.Model):
 
 
 class RecipeIngredient(models.Model):
-    ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s',
+    )
     amount = models.IntegerField(
         validators=[
             MinValueValidator(1, message=_('Must be more then 1')),
@@ -87,6 +106,8 @@ class RecipeIngredient(models.Model):
     )
 
     class Meta:
+        verbose_name = _('RecipeIngredient')
+        verbose_name_plural = _('RecipeIngredients')
         constraints = [
             models.UniqueConstraint(
                 fields=['recipe', 'ingredients'],
@@ -97,3 +118,60 @@ class RecipeIngredient(models.Model):
     def __str__(self):
         return f'{self.recipe} {self.ingredients}'
 
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s',
+    )
+
+    class Meta:
+        verbose_name = _('ShoppingCart')
+        verbose_name_plural = _('ShoppingCarts')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'user'],
+                name='%(app_label)s_%(class)s',
+            )
+        ]
+        indexes = [
+            models.Index(fields=('user',))
+        ]
+
+    def __str__(self):
+        return f'{self.user} {self.recipe}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s',
+    )
+
+    class Meta:
+        verbose_name = _('Favorite')
+        verbose_name_plural = _('Favorites')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'user'],
+                name='%(app_label)s_%(class)s',
+            )
+        ]
+        indexes = [
+            models.Index(fields=('user',))
+        ]
+
+    def __str__(self):
+        return f'{self.user} {self.recipe}'
