ee782adfec7881ed33b7b3fd445a50303e576030
bulatgabdullin <112638878+ByJIaT@users.noreply.github.com>
2023-07-12 22:33:16 +0500

    Refactoring

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,8 +4,8 @@ if [ "$DATABASE" = "postgres" ]
 then
   echo "Waiting for postgres..."
 
-  while ! nc -z localhost 5432;
-    do sleep 0.3
+  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT;
+    do sleep 0.1
   done
 
   echo "Postgres started"
